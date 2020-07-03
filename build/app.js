57982564fae5b3ec8fc802feb5d0feadcdf3af1f
washiPolly <pollylsu@gmail.com>
2020-07-03 15:18:40 -0700

    npm run build

---
+++ build/app.js
@@ -0,0 +1,7 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.static(__dirname));
+
+app.listen(process.env.PORT || 3000);
---
+++ build/precache-manifest.d3de6f56fa584df3ff94ed8b892c4a29.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "557565c44b61608ae1a94c2537884f43",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2cd84a5ee18356c0e13a",
+    "url": "/static/css/main.6e900ce7.chunk.css"
+  },
+  {
+    "revision": "d58bcaec15d18536ea88",
+    "url": "/static/js/2.5f21b655.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.5f21b655.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2cd84a5ee18356c0e13a",
+    "url": "/static/js/main.f4c876c1.chunk.js"
+  },
+  {
+    "revision": "73635f645c271e35ff15",
+    "url": "/static/js/runtime-main.83c3e0c4.js"
+  }
+]);
